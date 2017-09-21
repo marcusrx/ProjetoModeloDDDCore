9584b9e70662a8062f8c2ddaefbd6fe61ced3f5d
Marcus Vinicius de Souza Bahia <marcus.bahia@dmip01.sede>
2017-09-21 14:48:31 -0300

    Alterações

---
+++ ProjetoModelo.Domain.Core/Models/Gestao/CentroCusto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProjetoModelo.Domain.Core.Interfaces;
+
+namespace ProjetoModelo.Domain.Core.Models.Gestao
+{
+    public class CentroCusto : IModel<CentroCusto>, IDisposable
+    {
+        public int ID { get; set; }
+        public string CodCentroCusto { get; set; }
+        public DateTime PeriodoInicial { get; set; }
+        public DateTime PeriodoFinal { get; set; }
+        public bool OrdemCOntinua { get; set; }
+        public bool Ativo { get; set; }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
